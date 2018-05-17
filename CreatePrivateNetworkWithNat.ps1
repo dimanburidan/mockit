a6cabb490011dc963d760e9e3efb0cdf67a91605
Dmitry Mischenko <dmitrymis@outlook.com>
2018-05-17 20:09:25 +0700

    Added CreatePrivateNetworkWithNat.ps1 Added Convert-VMGeneration.ps1

---
+++ CreatePrivateNetworkWithNat.ps1
@@ -0,0 +1,14 @@
+﻿$PrivateNetworkVirtualSwitchExists = ((Get-VMSwitch | where {$_.name -eq "Private Network" -and $_.SwitchType -eq "Private"}).count -ne 0)
+
+# If statement to check if Private Switch already exists. If it does write a message to the host 
+# saying so and if not create Private Virtual Switch
+if ($PrivateNetworkVirtualSwitchExists -eq "True")
+{
+write-host "< Private Network >   ---- switch already Exists"
+} 
+else
+{
+$VMSwitch = New-VMSwitch -SwitchName "Private Network" -SwitchType Internal
+New-NetIPAddress -IPAddress 172.16.0.1 -PrefixLength 16 -InterfaceIndex $($(get-netadapter -Name "vEthernet ($($VMSwitch.Name))").ifIndex)
+New-NetNat -Name NatNetwork -InternalIPInterfaceAddressPrefix 172.16.0.0/16
+}
